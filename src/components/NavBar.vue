130917df8f4f894c92b93287efdcba10b1e4e64e
Trần Thiện Phú <hostcode0301@gmail.com>
2021-12-11 12:12:54 +0700

    Login; Navigation Guard; Save user data to Local Storage

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,98 +1,122 @@
 <template>
-    <nav
-        class="navbar navbar-expand-md navbar-dark bg-dark fixed-top"
-        aria-label="Fourth navbar example"
-    >
-        <div class="container-fluid">
-            <router-link class="navbar-brand" to="/">Lapis</router-link>
-            <button
-                class="navbar-toggler"
-                type="button"
-                data-bs-toggle="collapse"
-                data-bs-target="#navbarsExample04"
-                aria-controls="navbarsExample04"
-                aria-expanded="false"
-                aria-label="Toggle navigation"
-            >
-                <span class="navbar-toggler-icon"></span>
-            </button>
+  <nav
+    class="navbar navbar-expand-md navbar-dark bg-dark fixed-top"
+    aria-label="Fourth navbar example"
+  >
+    <div class="container-fluid my-0">
+      <router-link class="navbar-brand ms-3" to="/">Lapis</router-link>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarsExample04"
+        aria-controls="navbarsExample04"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
 
-            <div class="collapse navbar-collapse" id="navbarsExample04">
-                <ul class="navbar-nav ms-auto mb-2 mb-md-0">
-                    <li class="nav-item mx-3">
-                        <router-link class="nav-link" to="/">Home</router-link>
-                    </li>
-                    <li class="nav-item mx-3">
-                        <router-link class="nav-link" to="/about"
-                            >About</router-link
-                        >
-                    </li>
-                    <li class="nav-item mx-3">
-                        <router-link class="nav-link" to="/login"
-                            >Login</router-link
-                        >
-                    </li>
-                    <li class="nav-item mx-3">
-                        <router-link class="nav-link" to="/signup"
-                            >Sign up</router-link
-                        >
-                    </li>
+      <div class="collapse navbar-collapse" id="navbarsExample04">
+        <ul class="navbar-nav ms-auto mb-2 mb-md-0">
+          <li class="nav-item mx-3">
+            <router-link class="nav-link" to="/">Home</router-link>
+          </li>
+          <li class="nav-item mx-3">
+            <router-link class="nav-link" to="/about">About</router-link>
+          </li>
 
-                    <li class="nav-item ms-3">
-                        <div class="btn-group">
-                            <a
-                                class="nav-link dropdown-toggle"
-                                data-bs-toggle="dropdown"
-                                aria-expanded="false"
-                            >
-                                Profile
-                            </a>
-                            <ul class="dropdown-menu dropdown-menu-end">
-                                <li>
-                                    <a class="dropdown-item" href="#"
-                                        >Menu item</a
-                                    >
-                                </li>
-                                <li>
-                                    <a class="dropdown-item" href="#"
-                                        >Menu item</a
-                                    >
-                                </li>
-                                <li>
-                                    <a class="dropdown-item" href="#"
-                                        >Menu item</a
-                                    >
-                                </li>
-                            </ul>
-                        </div>
-                    </li>
+          <!-- Login & Logout buttn -->
+          <template v-if="userStore.isEmpty">
+            <li class="nav-item mx-3">
+              <router-link class="nav-link" to="/login">Login</router-link>
+            </li>
+            <li class="nav-item mx-3">
+              <router-link class="nav-link" to="/signup">Sign up</router-link>
+            </li>
+          </template>
+
+          <!-- Profile -->
+          <template v-else>
+            <li class="nav-item mx-3">
+              <router-link class="nav-link" to="/upload">Upload</router-link>
+            </li>
+            <li class="nav-item mx-3">
+              <div class="btn-group">
+                <a
+                  class="nav-link dropdown-toggle"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  <img :src="userStore.avatar" alt="user avatar" class="user_avatar" />
+                </a>
+                <ul class="dropdown-menu dropdown-menu-end">
+                  <li>
+                    <router-link class="dropdown-item" :to="`/user/${userStore.id}`"
+                      >Profile</router-link
+                    >
+                  </li>
+                  <li>
+                    <p class="dropdown-item my-0" @click="logout">Logout</p>
+                  </li>
                 </ul>
-            </div>
-        </div>
-    </nav>
+              </div>
+            </li>
+          </template>
+        </ul>
+      </div>
+    </div>
+  </nav>
 </template>
 
-<script>
-export default {};
+<script setup>
+// Store
+import { useUserStore } from "../store/useUser";
+
+// Router
+import { useRouter } from "vue-router";
+
+// Declare router & store
+const userStore = useUserStore();
+const router = useRouter();
+
+// Logout method
+const logout = () => {
+  // Clear user data
+  userStore.logout();
+  userStore.$reset();
+
+  // Redirect to Home
+  router.push("/");
+};
 </script>
 
-
 <style scoped>
+nav {
+  max-height: 56px;
+}
+
 .fixed-top {
-    z-index: 10;
+  z-index: 10;
 }
 
 .bd-placeholder-img {
-    font-size: 1.125rem;
-    text-anchor: middle;
-    -webkit-user-select: none;
-    -moz-user-select: none;
-    user-select: none;
+  font-size: 1.125rem;
+  text-anchor: middle;
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  user-select: none;
 }
 
 @media (min-width: 768px) {
-    .bd-placeholder-img-lg {
-        font-size: 3.5rem;
-    }
+  .bd-placeholder-img-lg {
+    font-size: 3.5rem;
+  }
+}
+
+.user_avatar {
+  border-radius: 50%;
+  height: 1.5rem;
+  width: 1.5rem;
 }
 </style>
--- src/store/useUser.js
+++ src/store/useUser.js
@@ -1,4 +1,5 @@
 import { defineStore } from 'pinia';
+import jwtDecode from 'jwt-decode';
 
 export const useUserStore = defineStore("userStore", {
     state: () => {
@@ -9,6 +10,44 @@ export const useUserStore = defineStore("userStore", {
             avatar: "",
             wallpaper: "",
             accessToken: ""
+        }
+    },
+    getters: {
+        isEmpty() {
+            if (this.id === "" && this.email === "" && this.name === "" && this.avatar === "" && this.wallpaper === "" && this.accessToken === "") {
+                return true;
+            }
+
+            return false;
+        },
+        localSave() {
+            try {
+                const localUser = JSON.parse(localStorage.getItem('user'));
+                const localToken = localStorage.getItem('accessToken');
+
+                // Check valid access token
+                if (!localToken) return null;
+
+                let decodedToken = jwtDecode(localToken);
+
+                if (decodedToken.exp * 1000 < Date.now()) {
+                    localStorage.removeItem("accessToken");
+                    localStorage.removeItem("user");
+                    return null;
+                }
+
+                const user = {
+                    id: localUser.id,
+                    email: localUser.email,
+                    name: localUser.name,
+                    avatar: localUser.avatar,
+                    wallpaper: localUser.wallpaper,
+                    accessToken: localToken
+                };
+                return user;
+            } catch {
+                return null;
+            }
         }
     },
     actions: {
@@ -22,12 +61,25 @@ export const useUserStore = defineStore("userStore", {
             this.wallpaper = newWallpaper;
         },
         setUserDetail(user) {
+            localStorage.setItem("accessToken", user.accessToken);
+            localStorage.setItem("user", JSON.stringify({
+                id: user.id,
+                email: user.email,
+                name: user.name,
+                avatar: user.avatar,
+                wallpaper: user.wallpaper,
+            }))
+
             this.id = user.id;
             this.email = user.email;
             this.name = user.name;
             this.avatar = user.avatar;
             this.wallpaper = user.wallpaper;
             this.accessToken = user.accessToken;
+        },
+        logout() {
+            localStorage.removeItem("accessToken");
+            localStorage.removeItem("user");
         }
     }
 });
