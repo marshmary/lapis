653c600ca2466c54ada48c942de4fa105f8d3c79
Trần Thiện Phú <44094072+hostcode0301@users.noreply.github.com>
2022-01-09 11:43:24 +0700

    Infinite scroll fixed (#14)
    * Replace masonry-layout lib; add Cache (#13)
    
    * Replace masonry-layout lib; add Cache

--- src/views/TheLogin.vue
+++ src/views/TheLogin.vue
@@ -8,7 +8,7 @@
 import ImageWrapper from "@/components/ImageWrapper.vue";
 import LoginForm from "@/components/LoginForm.vue";
 
-let imageurl = "https://i.ibb.co/jvXysdr/84908698-p1.jpg";
+let imageurl = "https://i.ibb.co/Z6DB73M/84908698-p1.jpg";
 </script>
 
 <style></style>
