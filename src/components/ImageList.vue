a08b1d53687530a30188759b83a12cd019e47b30
Trần Thiện Phú <44094072+hostcode0301@users.noreply.github.com>
2022-01-09 11:33:19 +0700

    Replace masonry-layout lib; add Cache (#13)
    * Replace masonry-layout lib; add Cache

--- src/components/ImageList.vue
+++ src/components/ImageList.vue
@@ -1,42 +1,40 @@
 <template>
   <section v-if="props.errors">
-    <!-- <p>Unable to find data</p> -->
     <not-found />
   </section>
 
-  <section v-else-if="props.loading">
-    <div class="row mx-0 pt-4">
-      <div class="col-12 col-md-6 col-lg-4 col-xxl-2 mb-4" v-for="n in 9" :key="n">
-        <image-list-item-loading />
+  <section v-else-if="props.data.length">
+    <div v-masonry transition-duration="0.3s" class="pt-4" item-selector=".item">
+      <div class="row mx-0 pt-4">
+        <div
+          v-masonry-tile
+          class="item col-12 col-md-6 col-lg-4 col-xxl-3"
+          v-for="item in props.data"
+          :key="item.id"
+        >
+          <image-list-item :image="item" class="mb-4" />
+        </div>
       </div>
     </div>
   </section>
 
-  <section v-else-if="props.data.length > 0">
-    <div class="row mx-0 pt-4 imagecontainer" data-masonry>
-      <div
-        class="col-12 col-md-6 col-lg-4 col-xxl-3 mb-4"
-        v-for="image in props.data"
-        :key="image.id"
-      >
-        <image-list-item :image="image" />
-      </div>
-    </div>
+  <section v-else-if="props.data.length === 0 && props.loading === false">
+    <not-found />
   </section>
 
-  <section v-else>
-    <not-found />
+  <section v-if="props.loading">
+    <div class="pt-4 text-center">
+      <div class="spinner-grow text-secondary" role="status">
+        <span class="visually-hidden">Loading...</span>
+      </div>
+    </div>
   </section>
 </template>
 
 <script setup>
-import { onUpdated } from "vue";
 import { defineProps } from "@vue/runtime-core";
-import Masonry from "masonry-layout";
-import imagesLoaded from "imagesloaded";
 
 // components
-import ImageListItemLoading from "@/components/ImageListItemLoading.vue";
 import ImageListItem from "@/components/ImageListItem.vue";
 import NotFound from "@/components/NotFound.vue";
 
@@ -44,18 +42,6 @@ const props = defineProps({
   data: Array,
   loading: Boolean,
   errors: Object,
-});
-
-onUpdated(() => {
-  imagesLoaded(document.querySelector(".imagecontainer"), function () {
-    if (props.loading === false) {
-      var row = document.querySelector("[data-masonry]");
-      new Masonry(row, {
-        // options
-        percentPosition: true,
-      });
-    }
-  });
 });
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+import { VueMasonryPlugin } from 'vue-masonry';
+
+import { ObserveVisibility } from 'vue-observe-visibility'
+
 // Tags input
 import VoerroTagsInput from '@voerro/vue-tagsinput';
 import '@voerro/vue-tagsinput/dist/style.css'
@@ -23,9 +27,22 @@ import { createPinia } from 'pinia';
 // add fontawesome icon to use
 library.add(faUserSecret, faClock, faDownload, faTags, faImage, faFileImage, faExclamationCircle, faCheckCircle, faArrowCircleUp, faTrash, faBars, faSearch, faTimes, faCheck)
 
+import Cache from './helpers/cacheHandler';
+
+Cache.init();
+
 createApp(App)
     .use(createPinia())
     .use(router)
+    .directive("observe-visibility", {
+        beforeMount: (el, binding, vnode) => {
+            vnode.context = binding.instance
+            ObserveVisibility.bind(el, binding, vnode)
+        },
+        updated: ObserveVisibility.update,
+        unmounted: ObserveVisibility.unbind,
+    })
+    .use(VueMasonryPlugin)
     .component("font-awesome-icon", <FontAwesomeIcon fixed-width></FontAwesomeIcon>)
     .component("tags-input", VoerroTagsInput)
     .mount('#app')
